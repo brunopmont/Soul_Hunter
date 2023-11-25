f915bb2dea7b4a3e68842c82b1e0d274f4035880
Bruno Porto <bpmonteiro@id.uff.br>
2023-11-25 13:15:12 -0300

    Vampiro Carioca Alfa 0.2
    Agora há spawn de zumbis, policiais e esqueletos de forma randômica, e o player já pode atacar e matar monstros.
    Além disso, agora alguns comandos estão organizados em funções.

--- AlphaVampiroCarioca.py
+++ AlphaVampiroCarioca.py
@@ -1,6 +1,7 @@
 from PPlay.window import *
 from PPlay.sprite import *
 from PPlay.gameimage import *
+from PPlay.collision import *
 import random
 
 janela = Window(1080, 720)
@@ -9,25 +10,44 @@ fundo = GameImage("png/fundo_jogo_3.png")
 janela.set_title("Vampiro Carioca")
 
 player = Sprite("png/player.png")
-esqueleto = Sprite("png/esqueleto.png")
-policial = Sprite("png/policial.png")
-
 player.set_position(460, 460)
-esqueleto.set_position(460, 10)
-policial.set_position(10, 460)
 
-def spawn_zombie(listacoordenadas):
-    zumbi = Sprite("png/zumbi.png")
+def spawn_mob(listacoordenadas): #spawn de mobs aleatórios, com suas posições definidas pra fora do mapa
+    listamob = [Sprite("png/zumbi.png"), Sprite("png/esqueleto.png"), Sprite("png/policial.png")]
+    i = random.randint(0, 2)
+    mob = listamob[i]
     coordenadas = random.choice(listacoordenadas)
-    zumbi.set_position(coordenadas[0], coordenadas[1])
-    return zumbi
+    mob.set_position(coordenadas[0], coordenadas[1])
+    return mob
+
+def colision_tiro_mob(listamob, listatiro): #testa a colisão do tiro do player com os mobs
+    if listamob != [] and listatiro != []:
+        for m in listamob:
+            for t in listatiro:
+                if Collision.collided(m, t):
+                    listamob.remove(m)
+                    listatiro.remove(t)
+                if t.x > janela.width:
+                    listatiro.remove(t)
+
+def mov_tiros(listatiros):
+    if listatiros != []:
+        for t in listatiros:
+            t.draw()
+    if listatiros != []:
+        for t in listatiros:
+            t.move_x(velTiro * janela.delta_time())
 
 velplayer = 200
 velmob = 50
 
-matZumbi = []
-contador = 0
-contadorgeral = 0
+matMob = []
+timerMob = 0
+timervelocidade = 0
+
+matTiros = []
+velTiro = 50
+timerhit = 0
 
 listacoordenadas = [[-25, -25], [-25, janela.height/2], [-25, janela.height + 25], [janela.width/2, janela.height + 25],  
                     [janela.width + 25, -25], [janela.width + 25, janela.height/2], [janela.width + 25, janela.height + 25], 
@@ -35,13 +55,17 @@ listacoordenadas = [[-25, -25], [-25, janela.height/2], [-25, janela.height + 25
 
 while True:
 
-    contadorgeral += janela.delta_time()
-    contador += janela.delta_time()
+    #CONTADORES
+    timervelocidade += janela.delta_time()
+    timerMob += janela.delta_time()
+    timerhit += janela.delta_time()
 
-    if contadorgeral > 30:
+    #INCREMENTADOR DE VELOCIDADE
+    if timervelocidade > 30:
         velmob = velmob + 20
-        contadorgeral = 0
+        timervelocidade = 0
 
+    #MOVIMENTAÇÃO E SPAWN DOS TIROS
     if teclado.key_pressed("w"):
         if player.y - 1 > 0: 
             player.move_y(janela.delta_time() * -velplayer)
@@ -54,28 +78,38 @@ while True:
     if teclado.key_pressed("d"):
         if player.x + 1 < janela.width - player.width: 
             player.move_x(janela.delta_time() * velplayer)
+    if teclado.key_pressed("space") and timerhit > 0.3:
+            tiro = Sprite("png/moeda.png")
+            tiro.set_position(player.x, player.y+player.height/2)
+            matTiros.append(tiro)
+            timerhit = 0
 
-    if contador > 1:
-        matZumbi.append(spawn_zombie(listacoordenadas))
-        contador = 0
+    #SPAWN E MOVIMENTAÇÃO MOBS
+    if timerMob > 1:
+        matMob.append(spawn_mob(listacoordenadas))
+        timerMob = 0
 
-    if matZumbi != []:
-        for z in matZumbi:
+    if matMob != []:
+        for z in matMob:
             z.draw()
 
-    if matZumbi != []:
-        for z in matZumbi:
+    if matMob != []:
+        for z in matMob:
             if z.x > player.x:
                 z.move_x(janela.delta_time() * -velmob)
             else:
                 z.move_x(janela.delta_time() * velmob)
-
             if z.y > player.y:
                 z.move_y(janela.delta_time() * -velmob)
             else:
                 z.move_y(janela.delta_time() * velmob)
 
-    janela.draw_text(str(contadorgeral) + " SEG",0,0,16,(255,255,255))
+    #MOVIMENTAÇÃO E COLISÃO DOS TIROS DO PLAYER
+    mov_tiros(matTiros)
+    colision_tiro_mob(matMob, matTiros)
+
+    #DESENHA INFORMAÇÕES AÍ
+    janela.draw_text(str(timervelocidade) + " SEG",0,0,16,(255,255,255))
     janela.draw_text(str(velmob) + " PIXEL/SEG",0,20,16,(255,255,255))
 
     janela.update()
